8818a7a4409ee776eeedace9941c4d42651e0b99
Fumble <serrier.jonathan@gmail.com>
2018-08-28 15:30:05 +0200

    installation doctrinefixtures pour généraion de database, créatin d'un loadUser

---
+++ src/P5/UserBundle/DataFixtures/ORM/LoadUser.php
@@ -0,0 +1,38 @@
+<?php
+// src/P5/UserBundle/DataFixtures/ORM/LoadUser.php
+
+namespace P5\UserBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use P5\UserBundle\Entity\User;
+
+class LoadUser implements FixtureInterface
+{
+  public function load(ObjectManager $manager)
+  {
+    // Les noms d'utilisateurs à créer
+    $listNames = array('Alexandre');
+
+    foreach ($listNames as $name) {
+      // On crée l'utilisateur
+      $user = new User;
+
+      // Le nom d'utilisateur et le mot de passe sont identiques pour l'instant
+      $user->setUsername($name);
+      $user->setPassword($name);
+
+      // On ne se sert pas du sel pour l'instant
+      $user->setSalt('');
+      // On définit uniquement le role ROLE_USER qui est le role de base
+      $user->setRoles(array('ROLE_MEMBRE'));
+
+      $user->setEmail("truc@truc.com");
+      // On le persiste
+      $manager->persist($user);
+    }
+
+    // On déclenche l'enregistrement
+    $manager->flush();
+  }
+}
