491202ac9e128b0b35d0a2efa8bdc9708258f544
Fumble <serrier.jonathan@gmail.com>
2018-08-29 20:06:39 +0200

    suppression de commentaire non adéquat

--- src/P5/UserBundle/DataFixtures/ORM/LoadUser.php
+++ src/P5/UserBundle/DataFixtures/ORM/LoadUser.php
@@ -18,13 +18,12 @@ class LoadUser implements FixtureInterface
       // On crée l'utilisateur
       $user = new User;
 
-      // Le nom d'utilisateur et le mot de passe sont identiques pour l'instant
+      
       $user->setUsername($name);
       $user->setPassword($name);
 
-      // On ne se sert pas du sel pour l'instant
       $user->setSalt('');
-      // On définit uniquement le role ROLE_USER qui est le role de base
+      
       $user->setRoles(array('ROLE_MEMBER'));
 
       $user->setEmail("truc@truc.com");
