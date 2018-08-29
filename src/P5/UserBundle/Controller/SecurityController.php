6560a7e4485edab7abfd8b39aa908e2443c6d2b7
Fumble <serrier.jonathan@gmail.com>
2018-08-29 20:05:42 +0200

    mise en place du controller, du formlaire, de la view pour l'inscription d'un membre, gestion du doublon d'utilisateur et d'email reste à gérer

--- src/P5/UserBundle/Controller/SecurityController.php
+++ src/P5/UserBundle/Controller/SecurityController.php
@@ -2,8 +2,17 @@
 
 namespace P5\UserBundle\Controller;
 
+use P5\UserBundle\Entity\User;
+use P5\UserBundle\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
 class SecurityController extends Controller
 {
@@ -21,5 +30,32 @@ class SecurityController extends Controller
       'error'         => $authenticationUtils->getLastAuthenticationError(),
     ));
   }
+
+  public function inscriptionAction(Request $request)
+  {
+  		$newmember = new User();
+  		$formbuilder = $this->get("form.factory")->createBuilder(UserType::class, $newmember);
+  		$inscriptionform = $formbuilder->getForm();
+
+  		if($request->isMethod("POST")){
+  			$inscriptionform->handleRequest($request);
+  				if($inscriptionform->isValid()) {
+
+  					$newmember->setRoles(array('ROLE_MEMBER'));
+  					$newmember->setSalt("");
+
+  					$em =$this->getDoctrine()->getManager();
+  					$em->persist($newmember);
+  					$em->flush();
+
+  					return $this->redirectToRoute('p5_core_home');
+  				}	
+  		}
+  
+  	return $this->render('P5UserBundle:Security:inscription.html.twig' , array(
+  		"inscriptionform" => $inscriptionform->createView()
+  	));
+  }
+
 
 }
