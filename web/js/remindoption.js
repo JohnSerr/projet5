8f99611b2fbb21768c8624a09c538327fa331cce
Fumble <serrier.jonathan@gmail.com>
2018-10-04 18:18:25 +0200

    implantation lib jquery , refonte du JS dynamisant le formulaire d'ajout ou d'edit de de rappel permettant de programmer un date de rappel

--- web/js/remindoption.js
+++ web/js/remindoption.js
@@ -1,18 +1,18 @@
+/* On met le selecteur de date invisible par défaut */
+
+$('#p5_todolistbundle_todolist_dateofend').parent().css("display", "none");
+
 /* On récupère la checkbox dans le DOM */
 
-let checkremind = $('checkbox');
+let checkremind = $('#p5_todolistbundle_todolist_remind');
 
+/* On implante un listener sur la checkbox */
 
-function remind() {	
-
-	if(checkremind == false) {
-
-		$('p5_todolistbundle_todolist_remind').css("display", "none");
-
-	} else if (checkremind == true) {
-
-		$('p5_todolistbundle_todolist_remind').css("display", "block");
-
+checkremind.change( function () {
+	if (checkremind.is(':checked')) {
+		$('#p5_todolistbundle_todolist_dateofend').parent().css("display", "block");
+	} else {
+		$('#p5_todolistbundle_todolist_dateofend').parent().css("display", "none");	
 	}
+});
 
-}
